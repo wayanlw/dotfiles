e827d7bd92b87d6f3f792aff1d665b3c66e3e426
wayan <wayanlw@gmail.com>
2021-07-03 13:39:20 +0200

    Sat Jul  3 01:39:20 PM CEST 2021

--- scripts/baseInstall/afterBase.sh
+++ scripts/baseInstall/afterBase.sh
@@ -30,6 +30,13 @@ sudo pacman -S --noconfirm --needed \
 		xdg-utils \
 		fzf
 		
+#install yay	
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+
 #yay -S --noconfirm \
 #		autokey-gtk \
 #		alttab-git
