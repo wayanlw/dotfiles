a9c06c3e17b8df54aee2cdb9d12d4a7e1f376d72
wayan <wayanlw@gmail.com>
2021-07-11 05:07:31 +1000

    move scripts folder

--- scripts/zshInstall.sh
+++ scripts/zshInstall.sh
@@ -6,3 +6,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 cp ~/.dotfiles/.zshrc ~/
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ 
