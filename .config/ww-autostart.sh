0bd59af0aaeb6983b8bc867901229490747f0cc2
Wayan LW <wayan@gmail.com>
2021-09-25 21:25:35 +1000

    Sat Sep 25 09:25:35 PM AEST 2021

--- .config/ww-autostart.sh
+++ .config/ww-autostart.sh
@@ -1,10 +1,10 @@
-
+#!/bin/bash
 clipmenud &
 dunst &
 lxpolkit &
 alttab -d 1 &
 nm-applet &
-pkill volumeicon; sleep 1; volumeicon;
+pkill volumeicon; sleep 1; volumeicon &
 feh --bg-scale --randomize ~/Pictures/wallpapers/*
 sxhkd -c ~/.dotfiles/.config/sxhkd/sxhkdrc &
 pkill tint2; sleep 1;tint2 &
@@ -18,4 +18,6 @@ autokey-gtk &
 lxpolkit &
 
 # dex is a autorun runner and craetor
-#dex -ae i3
+# dex -ae i3
+
+notify-send "autostart.sh completed"
