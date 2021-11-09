ce1e9960ba593dee7f71e7a1ca95f766d3b9ff14
Wayan LW <wayan@gmail.com>
2021-11-09 11:11:25 +1100

    changes

--- ww-scripts/windowplace-multi.sh
+++ ww-scripts/windowplace-multi.sh
@@ -10,39 +10,39 @@ newPosX=$((screenWidth/2-width/2))
 newPosY=$((screenHeight/2-height/2))
 
 # if there are multiple monitors have to use this use xrandr to identify the below parameters
-# use xrandr | grep ' \connected\ ' to get these information 
+# use xrandr | grep ' \connected\ ' to get these information
 
 # taskbar size
 taskbar=50
 
-# primary monitor
-w2=1920
-h2=1080
-
-# seconday monitor
+# top left monitor. Pixedl count starts from top left
 w1=1366
 h1=768
 
+# other monitor
+w2=1920
+h2=1080
+
 # offset
-xP=333
-yP=1080
+xP=1366
+yP=0
 
 
 case $1 in
     1) # Primary full screen
         wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
-        xdotool getactivewindow windowmove 0 0 
+        xdotool getactivewindow windowmove 0 0
         xdotool getactivewindow windowsize "$((w1))" "$((h1-taskbar))"
         ;;
     2) # Primary left half
         wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
-        xdotool getactivewindow windowmove 0 0 
+        xdotool getactivewindow windowmove 0 0
         xdotool getactivewindow windowsize "$((w1/2))" "$((h1-taskbar))"
         ;;
     3) # Primary second half
         wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
         xdotool getactivewindow windowsize "$((w1/2))" "$((h1-taskbar))"
-        xdotool getactivewindow windowmove "$((w1/2))" 0 
+        xdotool getactivewindow windowmove "$((w1/2))" 0
         ;;
     4) # secondary full screen
         wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
@@ -51,9 +51,9 @@ case $1 in
         ;;
     5) # secondary first half
         wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
-        # wmctrl -r :ACTIVE: -e 0,277,1080,$((w2/2)),750 
-        # wmctrl -r :ACTIVE: -e 0,$xP,1080,$((w2/2)),$h2 
-        xdotool getactivewindow windowmove --sync $xP $yP 
+        # wmctrl -r :ACTIVE: -e 0,277,1080,$((w2/2)),750
+        # wmctrl -r :ACTIVE: -e 0,$xP,1080,$((w2/2)),$h2
+        xdotool getactivewindow windowmove --sync $xP $yP
         xdotool getactivewindow windowsize --sync "$((w2/2))" "$((h2-taskbar))"
         ;;
     6) # Right half of the screen
