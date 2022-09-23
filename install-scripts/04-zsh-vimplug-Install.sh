f499b02a0d45343603dd58d58788c4a42b451bb8
xx@gmail.com <xx@gmail.com>
2022-09-23 18:10:43 +1000

    test

--- install-scripts/04-zsh-vimplug-Install.sh
+++ install-scripts/04-zsh-vimplug-Install.sh
@@ -7,14 +7,14 @@ chsh -s "$(which zsh)"
 
 [[ -f $HOME/.dotfiles/.zshrc ]] && ln -sf $HOME/.dotfiles/.zshrc $HOME/.zshrc && echo "####### .zshrc linked" || echo ".zshrc not available in ~/.dotfiles"
 
-# ──────────────────────────  Cloning Oh-my-Zsh Plugins ───────────────────────── #
-printf '\n>>>>>>> installing oh-my-zsh plugins\n'
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # ───────────────────────────── install oh-my-zsh ──────────────────────────── #
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp $HOME/.dotfiles/.zshrc $HOME/
 
+# ──────────────────────────  Cloning Oh-my-Zsh Plugins ───────────────────────── #
+printf '\n>>>>>>> installing oh-my-zsh plugins\n'
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 
