f9c62730a2147d90178cd4fba14bd385f1993cd4
ww <ww@ww.com>
2022-10-16 18:59:28 +1100

    adjusting for fedora

--- evremap-keybinder/enabler-script.sh
+++ evremap-keybinder/enabler-script.sh
@@ -1,5 +1,9 @@
 #~/bin/bash
-sudo cp $HOME/.dotfiles/evremap-keybinder/evremap.service /usr/lib/systemd/system/
+MYHOME="/home/ww"
+sudo systemctl stop evremap.service
+sudo cp $MYHOME/.dotfiles/evremap-keybinder/evremap.service /usr/lib/systemd/system/
+sudo cp ./evremap /usr/bin/evremap
+sudo cp ./ww-evremap.toml $MYHOME/.config/
 sudo systemctl daemon-reload
 sudo systemctl enable evremap.service
 sudo systemctl start evremap.service
